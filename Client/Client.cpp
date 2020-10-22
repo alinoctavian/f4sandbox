47f2eaed8b54fb297d5e7108bd6d99b2adb65fb2
alin <alinoctavian@protonmail.com>
2020-10-22 23:28:08 +0300

    Going to move to mono , probably

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,20 +1,199 @@
-// Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <Windows.h>
 #include <iostream>
+#include <limits.h>
 
-int main()
+#include <nethost.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <coreclr_delegates.h>
+#include <hostfxr.h>
+
+#define STR(s) L ## s
+#define CH(c) L ## c
+#define DIR_SEPARATOR L'\\'
+
+
+
+using string_t = std::basic_string<char_t>;
+
+namespace
 {
-    std::cout << "Hello World!\n";
+    // Globals to hold hostfxr exports
+    hostfxr_initialize_for_runtime_config_fn init_fptr;
+    hostfxr_get_runtime_delegate_fn get_delegate_fptr;
+    hostfxr_close_fn close_fptr;
+
+    // Forward declarations
+    bool load_hostfxr();
+    load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t* assembly);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main(int argc, wchar_t* argv[])
+{
+    std::cout << "Test" << std::endl;
+
+    char_t host_path[MAX_PATH];
+
+    auto size = ::GetFullPathNameW(argv[0], sizeof(host_path) / sizeof(char_t), host_path, nullptr);
+    assert(size != 0);
+
+    string_t root_path = host_path;
+    auto pos = root_path.find_last_of(DIR_SEPARATOR);
+    assert(pos != string_t::npos);
+    root_path = root_path.substr(0, pos + 1);
+
+    //
+    // STEP 1: Load HostFxr and get exported hosting functions
+    //
+    if (load_hostfxr())
+    {
+        assert(false && "Failure: load_hostfxr()");
+        return EXIT_FAILURE;
+    }
+
+    //
+    // STEP 2: Initialize and start the .NET Core runtime
+    //
+    const string_t config_path = root_path + STR("\\Debug\\TestApp.runtimeconfig.json");
+    load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer = nullptr;
+    load_assembly_and_get_function_pointer = get_dotnet_load_assembly(config_path.c_str());
+    assert(load_assembly_and_get_function_pointer == nullptr && "Failure: get_dotnet_load_assembly()");
+
+    //
+    // STEP 3: Load managed assembly and get function pointer to a managed method
+    //
+    const string_t dotnetlib_path = root_path + STR("TestApp.dll");
+    const char_t* dotnet_type = STR("TestApp.Lib, TestApp");
+    const char_t* dotnet_type_method = STR("Hello");
+    // <SnippetLoadAndGet>
+    // Function pointer to managed delegate
+    component_entry_point_fn hello = nullptr;
+    int rc = load_assembly_and_get_function_pointer(
+        dotnetlib_path.c_str(),
+        dotnet_type,
+        dotnet_type_method,
+        nullptr /*delegate_type_name*/,
+        nullptr,
+        (void**)&hello);
+    // </SnippetLoadAndGet>
+    assert(rc == 0 && hello != nullptr && "Failure: load_assembly_and_get_function_pointer()");
+
+    //
+    // STEP 4: Run managed code
+    //
+    struct lib_args
+    {
+        const char_t* message;
+        int number;
+    };
+    for (int i = 0; i < 3; ++i)
+    {
+        // <SnippetCallManaged>
+        lib_args args
+        {
+            STR("from host!"),
+            i
+        };
+
+        hello(&args, sizeof(args));
+        // </SnippetCallManaged>
+    }
+
+    // Function pointer to managed delegate with non-default signature
+    typedef void (CORECLR_DELEGATE_CALLTYPE* custom_entry_point_fn)(lib_args args);
+    custom_entry_point_fn custom = nullptr;
+    rc = load_assembly_and_get_function_pointer(
+        dotnetlib_path.c_str(),
+        dotnet_type,
+        STR("CustomEntryPoint") /*method_name*/,
+        STR("DotNetLib.Lib+CustomEntryPointDelegate, DotNetLib") /*delegate_type_name*/,
+        nullptr,
+        (void**)&custom);
+    assert(rc == 0 && custom != nullptr && "Failure: load_assembly_and_get_function_pointer()");
+
+    lib_args args
+    {
+        STR("from host!"),
+        -1
+    };
+    custom(args);
+
+
+    std::getchar();
+
+    return 0;
+}
+
+namespace
+{
+    // Forward declarations
+    void* load_library(const char_t*);
+    void* get_export(void*, const char*);
+
+    void* load_library(const char_t* path)
+    {
+        HMODULE h = ::LoadLibraryW(path);
+        assert(h != nullptr);
+        return (void*)h;
+    }
+    void* get_export(void* h, const char* name)
+    {
+        void* f = ::GetProcAddress((HMODULE)h, name);
+        assert(f != nullptr);
+        return f;
+    }
+
+
+    // <SnippetLoadHostFxr>
+    // Using the nethost library, discover the location of hostfxr and get exports
+    bool load_hostfxr()
+    {
+        // Pre-allocate a large buffer for the path to hostfxr
+        char_t buffer[MAX_PATH];
+        size_t buffer_size = sizeof(buffer) / sizeof(char_t);
+        int rc = get_hostfxr_path(buffer, &buffer_size, nullptr);
+        if (rc != 0)
+            return false;
+
+        // Load hostfxr and get desired exports
+        void* lib = load_library(buffer);
+        init_fptr = (hostfxr_initialize_for_runtime_config_fn)get_export(lib, "hostfxr_initialize_for_runtime_config");
+        get_delegate_fptr = (hostfxr_get_runtime_delegate_fn)get_export(lib, "hostfxr_get_runtime_delegate");
+        close_fptr = (hostfxr_close_fn)get_export(lib, "hostfxr_close");
+
+        return (init_fptr && get_delegate_fptr && close_fptr);
+    }
+    // </SnippetLoadHostFxr>
+
+    // <SnippetInitialize>
+    // Load and initialize .NET Core and get desired function pointer for scenario
+    load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t* config_path)
+    {
+        // Load .NET Core
+        void* load_assembly_and_get_function_pointer = nullptr;
+        hostfxr_handle cxt = nullptr;
+        int rc = init_fptr(config_path, nullptr, &cxt);
+        if (rc != 0 || cxt == nullptr)
+        {
+            std::cerr << "Init failed: " << std::hex << std::showbase << rc << std::endl;
+            close_fptr(cxt);
+            return nullptr;
+        }
+
+        // Get the load assembly function pointer
+        rc = get_delegate_fptr(
+            cxt,
+            hdt_load_assembly_and_get_function_pointer,
+            &load_assembly_and_get_function_pointer);
+        if (rc != 0 || load_assembly_and_get_function_pointer == nullptr)
+            std::cerr << "Get delegate failed: " << std::hex << std::showbase << rc << std::endl;
+
+        close_fptr(cxt);
+        return (load_assembly_and_get_function_pointer_fn)load_assembly_and_get_function_pointer;
+    }
+    // </SnippetInitialize>
+}
